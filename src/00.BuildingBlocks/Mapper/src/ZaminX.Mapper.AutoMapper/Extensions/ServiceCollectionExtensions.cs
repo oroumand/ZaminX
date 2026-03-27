a43234b49628327a1de8c0616db6058b0d9c6dce
Alireza Oroumand <ar.oroumand@gmail.com>
2026-03-27 12:36:58 +0330

    feat(mapper): add AutoMapper registration extension

--- src/00.BuildingBlocks/Mapper/src/ZaminX.Mapper.AutoMapper/Extensions/ServiceCollectionExtensions.cs
+++ src/00.BuildingBlocks/Mapper/src/ZaminX.Mapper.AutoMapper/Extensions/ServiceCollectionExtensions.cs
@@ -1,9 +1,19 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using ZaminX.Mapper.Abstractions.Contracts;
+using ZaminX.Mapper.AutoMapper.Adapters;
 
 namespace ZaminX.Mapper.AutoMapper.Extensions;
 
 public static class ServiceCollectionExtensions
 {
+    public static IServiceCollection AddZaminXAutoMapperAdapter(this IServiceCollection services)
+    {
+        services.AddAutoMapper(c=> { },typeof(ServiceCollectionExtensions).Assembly);
+        services.AddTransient<IMapperAdapter, AutoMapperAdapter>();
+
+        return services;
+    }
 }
