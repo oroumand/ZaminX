fd574d2f69491837d89d98e362e158633e9b9ad3
Alireza Oroumand <ar.oroumand@gmail.com>
2026-03-27 12:48:58 +0330

    feat(mapper): add AutoMapper options model

---
+++ src/00.BuildingBlocks/Mapper/src/ZaminX.Mapper.AutoMapper/Configurations/ZaminXAutoMapperOptions.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper;
+using System.Reflection;
+
+namespace ZaminX.Mapper.AutoMapper.Configurations;
+
+public sealed class ZaminXAutoMapperOptions
+{
+    public IList<Assembly> Assemblies { get; } = new List<Assembly>();
+
+    public Action<IMapperConfigurationExpression>? Configure { get; set; }
+}
