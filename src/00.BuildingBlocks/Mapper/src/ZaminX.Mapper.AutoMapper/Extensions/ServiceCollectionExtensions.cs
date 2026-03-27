5513e8f1c6d3411c0155d0361b015928a18e222e
Alireza Oroumand <ar.oroumand@gmail.com>
2026-03-27 12:52:43 +0330

    refactor(mapper): support options-based AutoMapper registration

--- src/00.BuildingBlocks/Mapper/src/ZaminX.Mapper.AutoMapper/Extensions/ServiceCollectionExtensions.cs
+++ src/00.BuildingBlocks/Mapper/src/ZaminX.Mapper.AutoMapper/Extensions/ServiceCollectionExtensions.cs
@@ -1,17 +1,30 @@
-﻿using AutoMapper;
+﻿using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
 using ZaminX.Mapper.Abstractions.Contracts;
 using ZaminX.Mapper.AutoMapper.Adapters;
+using ZaminX.Mapper.AutoMapper.Configurations;
 
 namespace ZaminX.Mapper.AutoMapper.Extensions;
 
 public static class ServiceCollectionExtensions
 {
+    public static IServiceCollection AddZaminXAutoMapperAdapter(this IServiceCollection services)
+    {
+        return services.AddZaminXAutoMapperAdapter(_ => { });
+    }
+
     public static IServiceCollection AddZaminXAutoMapperAdapter(
         this IServiceCollection services,
-        Action<IMapperConfigurationExpression>? configure = null)
+        Action<ZaminXAutoMapperOptions> configure)
     {
-        services.AddAutoMapper(configure ?? (_ => { }), typeof(ServiceCollectionExtensions).Assembly);
+        var options = new ZaminXAutoMapperOptions();
+        configure(options);
+
+        var assemblies = options.Assemblies.Count > 0
+            ? options.Assemblies.ToArray()
+            : [typeof(ServiceCollectionExtensions).Assembly];
+
+        services.AddAutoMapper(options.Configure ?? (_ => { }), assemblies);
         services.AddTransient<IMapperAdapter, AutoMapperAdapter>();
 
         return services;
