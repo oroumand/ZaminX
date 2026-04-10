5e0447e003c2b64938540e0c8a9ce85cb401eb90
Alireza Oroumand <ar.oroumand@gmail.com>
2026-04-10 21:04:48 +0330

    feat(relay): add query contracts

---
+++ src/00.BuildingBlocks/03.DomainAndApplicationPrimitives/Application/src/ZaminX.BuildingBlocks.Application/Queries/IQuery.cs
@@ -0,0 +1,8 @@
+﻿using ZaminX.BuildingBlocks.Application.Requests;
+using ZaminX.BuildingBlocks.Application.Results;
+
+namespace ZaminX.BuildingBlocks.Application.Queries;
+
+public interface IQuery<TResponse> : IRequest<Result<TResponse>>
+{
+}
---
+++ src/00.BuildingBlocks/03.DomainAndApplicationPrimitives/Application/tests/ZaminX.BuildingBlocks.Application.Tests/Queries/QueryContractsTests.cs
@@ -0,0 +1,29 @@
+﻿using ZaminX.BuildingBlocks.Application.Messages;
+using ZaminX.BuildingBlocks.Application.Queries;
+using ZaminX.BuildingBlocks.Application.Requests;
+using ZaminX.BuildingBlocks.Application.Results;
+
+namespace ZaminX.BuildingBlocks.Application.Tests.Queries;
+
+public class QueryContractsTests
+{
+    private sealed class TestQuery : IQuery<Guid>
+    {
+    }
+
+    [Fact]
+    public void Query_Should_Implement_Message()
+    {
+        var query = new TestQuery();
+
+        Assert.IsType<IMessage>(query, exactMatch: false);
+    }
+
+    [Fact]
+    public void Query_Should_Implement_Request_Of_ResultOfT()
+    {
+        var query = new TestQuery();
+
+        Assert.IsType<IRequest<Result<Guid>>>(query, exactMatch: false);
+    }
+}
