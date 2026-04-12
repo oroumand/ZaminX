8f1d4a5311e7b97a1c775991e51b6afe5de0064f
Alireza Oroumand <ar.oroumand@gmail.com>
2026-04-12 21:44:47 +0330

    feat(axiom): add read repository scanning support
    * add read repository registration helper
    * add read repository assembly scanning extension
    * align read registration with write registration approach
    * remove need for manual read repository registration

---
+++ src/00.BuildingBlocks/04.DataAndPersistence/Axiom/src/ZaminX.BuildingBlocks.Data.EntityFrameworkCore.Read/Extensions/EntityFrameworkCoreBuilderExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+using ZaminX.BuildingBlocks.Data.EntityFrameworkCore.Builders;
+using ZaminX.BuildingBlocks.Data.EntityFrameworkCore.Read.Internals;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class EntityFrameworkCoreBuilderExtensions
+{
+    public static EntityFrameworkCoreBuilder<TDbContext> ScanReadRepositoriesFromAssemblies<TDbContext>(
+    this EntityFrameworkCoreBuilder<TDbContext> builder,
+    params Assembly[] assemblies)
+    where TDbContext : DbContext
+    {
+        ArgumentNullException.ThrowIfNull(builder);
+        ArgumentNullException.ThrowIfNull(assemblies);
+
+
+    ReadRepositoryRegistrationHelper.RegisterRepositories(builder.Services, assemblies);
+
+        return builder;
+    }
+
+
+}
---
+++ src/00.BuildingBlocks/04.DataAndPersistence/Axiom/src/ZaminX.BuildingBlocks.Data.EntityFrameworkCore.Read/Internals/ReadRepositoryRegistrationHelper.cs
@@ -0,0 +1,95 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using ZaminX.BuildingBlocks.Data.Read.Abstractions.Contracts;
+
+namespace ZaminX.BuildingBlocks.Data.EntityFrameworkCore.Read.Internals;
+
+internal static class ReadRepositoryRegistrationHelper
+{
+    public static void RegisterRepositories(
+    IServiceCollection services,
+    params Assembly[] assemblies)
+    {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(assemblies);
+
+        foreach (var assembly in assemblies.Distinct())
+        {
+            var implementationTypes = assembly
+                .GetTypes()
+                .Where(type =>
+                    type is { IsClass: true, IsAbstract: false } &&
+                    IsEfReadRepository(type))
+                .ToList();
+
+            foreach (var implementationType in implementationTypes)
+            {
+                var serviceTypes = implementationType
+                    .GetInterfaces()
+                    .Where(IsReadRepositoryInterface)
+                    .ToList();
+
+                foreach (var serviceType in serviceTypes)
+                {
+                    RegisterScopedIfMissing(services, serviceType, implementationType);
+                }
+            }
+        }
+    }
+
+    private static bool IsEfReadRepository(Type type)
+    {
+        while (type.BaseType is not null)
+        {
+            var baseType = type.BaseType;
+
+            if (baseType.IsGenericType &&
+                baseType.GetGenericTypeDefinition() == typeof(EfReadRepository<,,>))
+            {
+                return true;
+            }
+
+            type = baseType;
+        }
+
+        return false;
+    }
+
+    private static bool IsReadRepositoryInterface(Type type)
+    {
+        if (!type.IsInterface)
+        {
+            return false;
+        }
+
+        if (type.IsGenericType &&
+            type.GetGenericTypeDefinition() == typeof(IReadRepository<,>))
+        {
+            return true;
+        }
+
+        return type.GetInterfaces()
+            .Any(interfaceType =>
+                interfaceType.IsGenericType &&
+                interfaceType.GetGenericTypeDefinition() == typeof(IReadRepository<,>));
+    }
+
+    private static void RegisterScopedIfMissing(
+        IServiceCollection services,
+        Type serviceType,
+        Type implementationType)
+    {
+        var alreadyRegistered = services.Any(service =>
+            service.ServiceType == serviceType &&
+            service.ImplementationType == implementationType);
+
+        if (alreadyRegistered)
+        {
+            return;
+        }
+
+        services.AddScoped(serviceType, implementationType);
+    }
+
+
+}
