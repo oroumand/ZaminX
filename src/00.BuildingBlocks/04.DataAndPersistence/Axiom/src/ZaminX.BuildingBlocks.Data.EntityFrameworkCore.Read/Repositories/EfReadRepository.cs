85c53932496b87aa7d99a06ac59191edfe64c725
Alireza Oroumand <ar.oroumand@gmail.com>
2026-04-12 21:27:39 +0330

    feat(axiom): add default sorting strategy to EF read repository
    * introduce ApplyDefaultSorting hook in base read repository
    * add fallback sorting by Id descending when no SortBy is provided
    * keep paging helper independent from default sorting logic
    * allow repositories to override default sorting behavior

--- src/00.BuildingBlocks/04.DataAndPersistence/Axiom/src/ZaminX.BuildingBlocks.Data.EntityFrameworkCore.Read/Repositories/EfReadRepository.cs
+++ src/00.BuildingBlocks/04.DataAndPersistence/Axiom/src/ZaminX.BuildingBlocks.Data.EntityFrameworkCore.Read/Repositories/EfReadRepository.cs
@@ -1,10 +1,9 @@
-﻿using System.Linq.Expressions;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using System.Reflection;
 using ZaminX.BuildingBlocks.Data.Abstractions.Models;
 using ZaminX.BuildingBlocks.Data.EntityFrameworkCore.Read.Extensions;
 using ZaminX.BuildingBlocks.Data.Read.Abstractions.Contracts;
-
-namespace ZaminX.BuildingBlocks.Data.EntityFrameworkCore.Read.Repositories;
 
 public abstract class EfReadRepository<TEntity, TId, TDbContext>(TDbContext dbContext) :
 IReadRepository<TEntity, TId>
@@ -20,6 +19,12 @@ where TDbContext : DbContext
         CancellationToken cancellationToken = default)
     {
         var query = CreatePagedQuery(request);
+
+        // 👇 فقط وقتی SortBy نداشت
+        if (string.IsNullOrWhiteSpace(request.SortBy))
+        {
+            query = ApplyDefaultSorting(query);
+        }
 
         return query.ToPagedResultAsync(request, cancellationToken);
     }
@@ -42,6 +47,40 @@ where TDbContext : DbContext
         return Entities.AsNoTracking();
     }
 
+    /// <summary>
+    /// Default sorting hook (fallback when no SortBy is provided)
+    /// </summary>
+    protected virtual IQueryable<TEntity> ApplyDefaultSorting(IQueryable<TEntity> query)
+    {
+        return TryApplyIdDescending(query);
+    }
+
     protected abstract Expression<Func<TEntity, bool>> BuildIdPredicate(TId id);
+
+    // 👇 internal helper
+    private static IQueryable<TEntity> TryApplyIdDescending(IQueryable<TEntity> query)
+    {
+        var property = typeof(TEntity)
+            .GetProperty("Id", BindingFlags.Public | BindingFlags.Instance);
+
+        if (property is null)
+        {
+            return query;
+        }
+
+        var parameter = Expression.Parameter(typeof(TEntity), "entity");
+        var propertyAccess = Expression.Property(parameter, property);
+        var lambda = Expression.Lambda(propertyAccess, parameter);
+
+        var methodCall = Expression.Call(
+            typeof(Queryable),
+            "OrderByDescending",
+            [typeof(TEntity), property.PropertyType],
+            query.Expression,
+            Expression.Quote(lambda));
+
+        return query.Provider.CreateQuery<TEntity>(methodCall);
+    }
+
 
 }
