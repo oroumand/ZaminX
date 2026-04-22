28d5802d20e4eed53d41a8a8ffc54fc2a284ec34
Alireza Oroumand <ar.oroumand@gmail.com>
2026-04-22 16:22:09 +0330

    feat(handler-execution): add command handler base

--- src/02. ApplicationPatterns/HandlerExecution/src/ZaminX.ApplicationPatterns.HandlerExecution/CommandHandlerBase.cs
+++ src/02. ApplicationPatterns/HandlerExecution/src/ZaminX.ApplicationPatterns.HandlerExecution/CommandHandlerBase.cs
@@ -1,9 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ZaminX.BuildingBlocks.Application.Commands;
+using ZaminX.BuildingBlocks.Application.Results;
+using ZaminX.BuildingBlocks.Data.Write.Abstractions.Contracts;
+using ZaminX.BuildingBlocks.Domain.Entities;
+
 
 namespace ZaminX.ApplicationPatterns.HandlerExecution;
 
-internal class CommandHandlerBase
+public abstract class CommandHandlerBase<TCommand, TRepository, TAggregate, TId> :
+    ApplicationHandlerBase,
+    ICommandHandler<TCommand>
+    where TCommand : ICommand
+    where TRepository : IWriteRepository<TAggregate, TId>
+    where TAggregate : AggregateRoot<TId>
 {
+    protected CommandHandlerBase(
+        TRepository repository,
+        IUnitOfWork unitOfWork,
+        HandlerServices services)
+        : base(services)
+    {
+        ArgumentNullException.ThrowIfNull(repository);
+        ArgumentNullException.ThrowIfNull(unitOfWork);
+
+        Repository = repository;
+        UnitOfWork = unitOfWork;
+    }
+
+    protected TRepository Repository { get; }
+
+    protected IUnitOfWork UnitOfWork { get; }
+
+    public abstract Task<Result> Handle(
+        TCommand command,
+        CancellationToken cancellationToken = default);
+}
+
+public abstract class CommandHandlerBase<TCommand, TResponse, TRepository, TAggregate, TId> :
+    ApplicationHandlerBase,
+    ICommandHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+    where TRepository : IWriteRepository<TAggregate, TId>
+    where TAggregate : AggregateRoot<TId>
+{
+    protected CommandHandlerBase(
+        TRepository repository,
+        IUnitOfWork unitOfWork,
+        HandlerServices services)
+        : base(services)
+    {
+        ArgumentNullException.ThrowIfNull(repository);
+        ArgumentNullException.ThrowIfNull(unitOfWork);
+
+        Repository = repository;
+        UnitOfWork = unitOfWork;
+    }
+
+    protected TRepository Repository { get; }
+
+    protected IUnitOfWork UnitOfWork { get; }
+
+    public abstract Task<Result<TResponse>> Handle(
+        TCommand command,
+        CancellationToken cancellationToken = default);
 }
