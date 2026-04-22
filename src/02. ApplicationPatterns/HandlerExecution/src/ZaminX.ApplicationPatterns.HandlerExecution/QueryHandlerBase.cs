a7b6f0358c8d436cb11787cbe5ab1e1774d01417
Alireza Oroumand <ar.oroumand@gmail.com>
2026-04-22 16:28:31 +0330

    feat(handler-execution): add query handler base

--- src/02. ApplicationPatterns/HandlerExecution/src/ZaminX.ApplicationPatterns.HandlerExecution/QueryHandlerBase.cs
+++ src/02. ApplicationPatterns/HandlerExecution/src/ZaminX.ApplicationPatterns.HandlerExecution/QueryHandlerBase.cs
@@ -1,9 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using ZaminX.BuildingBlocks.Application.Queries;
+using ZaminX.BuildingBlocks.Application.Results;
+using ZaminX.BuildingBlocks.Data.Read.Abstractions.Contracts;
+using ZaminX.BuildingBlocks.Domain.Entities;
 
 namespace ZaminX.ApplicationPatterns.HandlerExecution;
 
-internal class QueryHandlerBase
+public abstract class QueryHandlerBase<TQuery, TResponse, TRepository, TEntity, TId> :
+    ApplicationHandlerBase,
+    IQueryHandler<TQuery, TResponse>
+    where TQuery : IQuery<TResponse>
+    where TRepository : IReadRepository<TEntity, TId>
+    where TEntity : Entity<TId>
 {
+    protected QueryHandlerBase(
+        TRepository repository,
+        HandlerServices services)
+        : base(services)
+    {
+        ArgumentNullException.ThrowIfNull(repository);
+
+        Repository = repository;
+    }
+
+    protected TRepository Repository { get; }
+
+    public abstract Task<Result<TResponse>> Handle(
+        TQuery query,
+        CancellationToken cancellationToken = default);
 }
