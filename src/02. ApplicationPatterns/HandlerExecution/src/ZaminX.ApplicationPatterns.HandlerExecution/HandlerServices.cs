a4942dc14dc99673e9cedbcb49e48d3659f43234
Alireza Oroumand <ar.oroumand@gmail.com>
2026-04-22 15:47:32 +0330

    feat(handler-execution): add handler services bundle

--- src/02. ApplicationPatterns/HandlerExecution/src/ZaminX.ApplicationPatterns.HandlerExecution/HandlerServices.cs
+++ src/02. ApplicationPatterns/HandlerExecution/src/ZaminX.ApplicationPatterns.HandlerExecution/HandlerServices.cs
@@ -1,9 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.Extensions.Logging;
+using ZaminX.BuildingBlocks.CrossCutting.ObjectMapper.Abstractions.Contracts;
+using ZaminX.BuildingBlocks.CrossCutting.Serializer.Abstractions.Contracts;
+using ZaminX.BuildingBlocks.CrossCutting.Translator.Abstractions.Contracts;
+using ZaminX.BuildingBlocks.IdentityAndUsers.Persona.Abstractions.Contracts;
 
 namespace ZaminX.ApplicationPatterns.HandlerExecution;
 
-internal class HandlerServices
+
+public sealed class HandlerServices
 {
+    public HandlerServices(
+        IMapperAdapter mapper,
+        IJsonSerializer serializer,
+        ITranslator translator,
+        ILoggerFactory loggerFactory,
+        ICurrentUser? currentUser = null)
+    {
+        ArgumentNullException.ThrowIfNull(mapper);
+        ArgumentNullException.ThrowIfNull(serializer);
+        ArgumentNullException.ThrowIfNull(translator);
+        ArgumentNullException.ThrowIfNull(loggerFactory);
+
+        Mapper = mapper;
+        Serializer = serializer;
+        Translator = translator;
+        LoggerFactory = loggerFactory;
+        CurrentUser = currentUser;
+    }
+
+    public IMapperAdapter Mapper { get; }
+
+    public IJsonSerializer Serializer { get; }
+
+    public ITranslator Translator { get; }
+
+    public ILoggerFactory LoggerFactory { get; }
+
+    public ICurrentUser? CurrentUser { get; }
 }
